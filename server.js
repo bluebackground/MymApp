02ef88a7da54d2f4957ba40896d2d5307aaf6cc1
Jonathan Chiang <signup.jc13@gmail.com>
2018-02-02 15:17:13 -0600

    tweak cors middleware options

--- server.js
+++ server.js
@@ -10,18 +10,19 @@ const {
   FRONT_END_SERVER
 } = require('./variables/connections.js');
 
-var whitelist = [FRONT_END_SERVER];
-var corsOptions = {
-  origin: function (origin, callback) {
-    if (whitelist.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  }
-}
+// var whitelist = [FRONT_END_SERVER];
+// var corsOptions = {
+//   origin: function (origin, callback) {
+//     if (whitelist.indexOf(origin) !== -1) {
+//       callback(null, true);
+//     } else {
+//       callback(new Error('Not allowed by CORS'));
+//     }
+//   }
+// }
 
-server.use(cors(corsOptions));
+// server.use(cors(corsOptions));
+server.user(cors({origin: '*'}));
 routes(server);
 
 module.exports = server;
