e73ec63e6af28a01fe62ffd62957820bfa2c9985
Jonathan Chiang <signup.jc13@gmail.com>
2018-02-02 15:44:21 -0600

    adding Access Control Headers

--- server.js
+++ server.js
@@ -22,9 +22,15 @@ const {
 // }
 
 // server.use(cors(corsOptions));
-server.use(cors({
-  origin: '*'
-}));
+// server.use(cors({
+//   origin: '*'
+// }));
+server.use(cors());
+server.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 routes(server);
 
