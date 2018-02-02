99c2ef8aa491d5902d374cd8f83f4a10c32dd7b5
Jonathan Chiang <signup.jc13@gmail.com>
2018-02-02 16:10:29 -0600

    tweak cors

--- server.js
+++ server.js
@@ -22,10 +22,10 @@ const {
 // }
 
 // server.use(cors(corsOptions));
-// server.use(cors({
-//   origin: '*'
-// }));
-server.use(cors());
+server.use(cors({
+  origin: '*'
+}));
+// server.use(cors());
 // server.use(function (req, res, next) {
 //   res.header("Access-Control-Allow-Origin", "*");
 //   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
@@ -35,18 +35,17 @@ server.use(cors());
 server.use(function (req, res, next) {
 
   // Website you wish to allow to connect
-  res.header('Access-Control-Allow-Origin', "*");
+  res.setHeader('Access-Control-Allow-Origin', '*');
 
   // Request methods you wish to allow
-  // res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 
   // Request headers you wish to allow
-  // res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
 
   // Set to true if you need the website to include cookies in the requests sent
   // to the API (e.g. in case you use sessions)
-  // res.setHeader('Access-Control-Allow-Credentials', true);
+  res.setHeader('Access-Control-Allow-Credentials', true);
 
   // Pass to next layer of middleware
   next();
