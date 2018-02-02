a9db3118fa07c6e01b3c88d7e6e6ca3842115d7c
Jonathan Chiang <signup.jc13@gmail.com>
2018-02-02 15:06:15 -0600

    adding cors white list frontend server

--- server.js
+++ server.js
@@ -5,7 +5,23 @@ const routes = require('./routes/index.js');
 
 const server = express();
 server.use(bodyParser.json());
-server.use(cors());
+
+const {
+  FRONT_END_SERVER
+} = require('./variables/connections.js');
+
+var whitelist = [FRONT_END_SERVER];
+var corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  }
+}
+
+server.use(cors(corsOptions));
 routes(server);
 
 module.exports = server;
---
+++ variables/connections.js
@@ -0,0 +1,5 @@
+const FRONT_END_SERVER = 'https://secure-gorge-57528.herokuapp.com/';
+
+module.exports = {
+  FRONT_END_SERVER
+};
